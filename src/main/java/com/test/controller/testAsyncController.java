a650ac090a3d6ae653c311cff780c69ffc1ae733
liwei1 <liwei1@akulaku.com>
2021-05-25 17:52:09 +0800

    mergedemo2

--- src/main/java/com/test/controller/testAsyncController.java
+++ src/main/java/com/test/controller/testAsyncController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/testAsyncController")
 public class testAsyncController {
 
-    //这个是测试merge
+    //这个是测试merge2
 
 //    @Autowired
     private AsyncService asyncService;
